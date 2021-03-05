c97db22f191de36ee42332eb23448659776374fc
Daniel Santos Dantas <dansdantas@gmail.com>
2021-03-05 08:10:05 -0300

    track external filesystem and remove commented lines

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env sh
 
-## Add this to your wm startup file.
-
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
+# Select monitor
+monitor=HDMI-0
+
+# Launch bars
 # MONITOR=eDP-1-1 polybar top -c ~/.config/polybar/config-top.ini &
-MONITOR=eDP-1-1 polybar bottom -c ~/.config/polybar/config-bottom.ini &
+MONITOR=$monitor polybar bottom -c ~/.config/polybar/config-bottom.ini &
