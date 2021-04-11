1b15460496910ba420290a5ee7456900524021f3
Daniel Santos Dantas <dansdantas@gmail.com>
2021-04-10 23:21:48 -0300

    add triple monitor script for faster setup

---
+++ .local/share/screenlayout/triple-monitor.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+xrandr --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal
+xrandr --output DP-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal
+xrandr --output DP-1 --off
+xrandr --output DP-2 --mode 1920x1080 --pos 3840x0 --rotate normal
+xrandr --output DP-3 --off
+xrandr --output DP-4 --off
+xrandr --output DP-5 --off
+
+# force workspaces to start on middle monitor
+bpsc monitor DP-0 -s HDMI-0
+
+# relaunch polybar on correct monitor
+setsid -f $HOME/.config/polybar/launch.sh > /dev/null 2>&1
