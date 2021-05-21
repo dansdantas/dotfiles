b1d879219f3ff7feb9a43096fcaaf777f82e649f
Daniel Santos Dantas <dansdantas@gmail.com>
2021-05-21 08:39:46 -0300

    move monitor config on bspwm to after setting up with xrandr

--- .local/share/screenlayout/work-horizontal.sh
+++ .local/share/screenlayout/work-horizontal.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-xrandr --output HDMI-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output eDP-1-1 --mode 1920x1080 --pos 1920x0 --rotate normal
+
+xrandr --output HDMI-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+xrandr --output eDP-1-1 --mode 1920x1080 --pos 1920x0 --rotate normal
+
+bspc monitor eDP-1-1 -d 1 2 3 4 5 6 7
+bspc monitor HDMI-0 -d 8 9
+bspc monitor eDP-1-1 -s HDMI-0
+
+# relaunch polybar on correct monitor
+setsid -f $HOME/.config/polybar/launch.sh > /dev/null 2>&1
--- .local/share/screenlayout/work-vertical.sh
+++ .local/share/screenlayout/work-vertical.sh
@@ -5,4 +5,9 @@ eDP=eDP-1-1
 xrandr --output $eDP --primary --mode 1920x1080 --pos 0x1080 --rotate normal
 xrandr --output $HDMI --mode 1920x1080 --pos 0x0 --rotate normal
 
-~/.config/polybar/launch.sh
+bspc monitor eDP-1-1 -d 1 2 3 4 5 6 7
+bspc monitor HDMI-0 -d 8 9
+bspc monitor eDP-1-1 -s HDMI-0
+
+# relaunch polybar on correct monitor
+setsid -f $HOME/.config/polybar/launch.sh > /dev/null 2>&1
