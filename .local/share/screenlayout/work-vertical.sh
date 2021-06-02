2a0262e5658d1896b2284c0bec8e549d1c782ad4
Daniel Santos Dantas <dansdantas@gmail.com>
2021-06-02 10:11:31 -0300

    correctly set up vertical layout

--- .local/share/screenlayout/work-vertical.sh
+++ .local/share/screenlayout/work-vertical.sh
@@ -5,8 +5,8 @@ eDP=eDP-1-1
 xrandr --output $eDP --primary --mode 1920x1080 --pos 0x1080 --rotate normal
 xrandr --output $HDMI --mode 1920x1080 --pos 0x0 --rotate normal
 
-bspc monitor eDP-1-1 -d 1 2 3 4 5 6 7
-bspc monitor HDMI-0 -d 8 9
+bspc monitor HDMI-0 -d 1 2 3 4 5 6 7
+bspc monitor eDP-1-1 -d 8 9
 bspc monitor eDP-1-1 -s HDMI-0
 
 # relaunch polybar on correct monitor
