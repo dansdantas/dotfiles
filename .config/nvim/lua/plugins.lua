6b568b6fd6a0042277c4fbf65334bc7c35b89a25
Daniel Santos Dantas <dansdantas@gmail.com>
2022-05-16 07:53:18 -0300

    add bootstrap for plugin manager and move use inside function

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,7 +1,10 @@
-vim.cmd [[packadd packer.nvim]]
-local use = require('packer').use
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  PackerBootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
 
-return require('packer').startup(function()
+return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
