583f9afb3ce3c9c7eee0ce3768fcf1275bd2c86d
Daniel Santos Dantas <dansdantas@gmail.com>
2022-05-16 10:32:47 -0300

    add base init.lua as config.lua

---
+++ .config/nvim/config.lua
@@ -0,0 +1,14 @@
+require('settings')
+
+require('maps')
+require('commands')
+require('plugins')
+
+require('plugins.settings')
+require('plugins.statusline')
+require('plugins.bufferline')
+require('plugins.lsp')
+require('plugins.coc')
+require('plugins.hop')
+require('plugins.telescope')
+require('plugins.treesitter')
