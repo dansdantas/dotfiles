2f98c59c1456df5927dd8c9271d33eb586c7cf3a
Daniel Santos Dantas <dansdantas@gmail.com>
2022-05-16 07:52:42 -0300

    add auto command to update plugins on filesave

--- .config/nvim/lua/commands.lua
+++ .config/nvim/lua/commands.lua
@@ -1,4 +1,13 @@
-vim.api.nvim_create_autocmd({'BufRead', 'BufNewFile'}, {
+local api = vim.api
+
+api.nvim_create_autocmd({'BufRead', 'BufNewFile'}, {
   pattern = {'*.arb'},
   command = 'setfiletype ruby'
+})
+
+local packer_user_config = api.nvim_create_augroup('PackerUserConfig', {})
+api.nvim_create_autocmd({'BufWritePost'}, {
+  pattern = {'plugins.lua'},
+  command = 'source <afile> | PackerCompile',
+  group = packer_user_config
 })
