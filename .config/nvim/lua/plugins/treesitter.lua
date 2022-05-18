17e89ad8c967f1f1d5ce5fc8396bb8396908f44d
Daniel Santos Dantas <dansdantas@gmail.com>
2022-05-18 16:05:54 -0300

    enable treesitter with all configs

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,9 +1,91 @@
--- require('nvim-treesitter.configs').setup({
---   ensure_installed = 'maintained',
---   highlight = {
---     enable = true,
---   },
---   indent = {
---     enable = true,
---   }
--- })
+local languages = {
+  'bash',
+  'c',
+  'c_sharp',
+  'cmake',
+  'comment',
+  'css',
+  'dockerfile',
+  'dot',
+  'elixir',
+  'elm',
+  'elvish',
+  'erlang',
+  'fennel',
+  'fish',
+  'go',
+  'graphql',
+  'html',
+  'http',
+  'java',
+  'javascript',
+  'jsdoc',
+  'json',
+  'json5',
+  'JSON',
+  'lua',
+  'make',
+  'markdown',
+  'python',
+  'r',
+  'regex',
+  'ruby',
+  'rust',
+  'scss',
+  'typescript',
+  'vim',
+  'vue',
+  'yaml',
+}
+
+require('nvim-treesitter.configs').setup {
+  ensure_installed = languages,
+  highlight = {
+    enable = true, -- false will disable the whole extension
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = 'gnn',
+      node_incremental = 'grn',
+      scope_incremental = 'grc',
+      node_decremental = 'grm',
+    },
+  },
+  indent = {
+    enable = true,
+  },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['ac'] = '@class.outer',
+        ['ic'] = '@class.inner',
+      },
+    },
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        [']m'] = '@function.outer',
+        [']]'] = '@class.outer',
+      },
+      goto_next_end = {
+        [']M'] = '@function.outer',
+        [']['] = '@class.outer',
+      },
+      goto_previous_start = {
+        ['[m'] = '@function.outer',
+        ['[['] = '@class.outer',
+      },
+      goto_previous_end = {
+        ['[M'] = '@function.outer',
+        ['[]'] = '@class.outer',
+      },
+    },
+  },
+}
