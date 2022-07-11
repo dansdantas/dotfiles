66dad0d2ce59eb2c35fd7babadf0fc44897c39f3
Daniel Santos Dantas <dansdantas@gmail.com>
2022-07-11 11:54:37 -0300

    use telescope dropdown to select directories to remove and refactor helpers

--- .config/nvim/lua/workspace.lua
+++ .config/nvim/lua/workspace.lua
@@ -1,9 +1,17 @@
 ---@diagnostic disable: undefined-doc-name
 local vfn = vim.fn
 
+local pickers = require "telescope.pickers"
+local finders = require "telescope.finders"
+local conf = require("telescope.config").values
+local actions = require "telescope.actions"
+local action_state = require "telescope.actions.state"
+
 local M = {}
 
 M.search_dirs = {}
+
+-- Helpers
 
 ---@private
 --- Returns nil if {status} is false or nil, otherwise returns the rest of the
@@ -26,6 +34,8 @@ local function npcall(fn, ...)
   return ok_or_nil(pcall(fn, ...))
 end
 
+-- Internal functions
+
 local get_workspace_from_user = function(workspace_folder)
   -- Start from current dir and not file name
   workspace_folder = workspace_folder or npcall(vfn.input, 'Folder: ', vfn.expand('%:p:h'), 'dir')
@@ -41,6 +51,46 @@ local get_workspace_from_user = function(workspace_folder)
   return workspace_folder
 end
 
+local remove_dir_from_list = function (dir)
+  if not dir then
+    return
+  end
+
+  local uri = vim.uri_from_fname(dir)
+  M.search_dirs[uri] = nil
+end
+
+local list_dirs = function ()
+  local dirs = {}
+
+  for _, folder in pairs(M.search_dirs) do
+    table.insert(dirs, folder.name)
+  end
+
+  return dirs
+end
+
+local list_dirs_on_telescope_picker = function(opts)
+  opts = opts or {}
+  pickers.new(opts, {
+    prompt_title = "workspaces",
+    finder = finders.new_table {
+      results = list_dirs(),
+    },
+    sorter = conf.generic_sorter(opts),
+    attach_mappings = function(prompt_bufnr)
+      actions.select_default:replace(function()
+        actions.close(prompt_bufnr)
+        local selection = action_state.get_selected_entry()
+        remove_dir_from_list(selection[1])
+      end)
+      return true
+    end,
+  }):find()
+end
+
+-- Exposed actions
+
 M.add_dir = function(workspace_folder)
   workspace_folder = get_workspace_from_user(workspace_folder)
 
@@ -52,20 +102,12 @@ M.add_dir = function(workspace_folder)
   M.search_dirs[uri] = { name = workspace_folder }
 end
 
-M.remove_dir = function(workspace_folder)
-  -- TODO: list already added workspaces instead of searching
-  workspace_folder = get_workspace_from_user(workspace_folder)
-
-  if not workspace_folder then
-    return
-  end
-
-  local uri = vim.uri_from_fname(workspace_folder)
-  M.search_dirs[uri] = nil
+M.remove_dir = function()
+  list_dirs_on_telescope_picker(require("telescope.themes").get_dropdown{})
 end
 
 M.list_dirs = function()
-  return M.search_dirs
+  return list_dirs()
 end
 
 M.clean_dirs = function()
@@ -73,13 +115,8 @@ M.clean_dirs = function()
 end
 
 M.find_files = function()
-  local search_dirs = {}
-  for _, folder in pairs(M.search_dirs) do
-    table.insert(search_dirs, folder.name)
-  end
-
   return require("telescope.builtin").find_files {
-    search_dirs = search_dirs,
+    search_dirs = list_dirs(),
     width = .25
   }
 end
