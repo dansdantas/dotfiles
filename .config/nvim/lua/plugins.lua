c3452489191ab77b3879e6236c14c362acc522bc
Daniel Santos Dantas <dansdantas@gmail.com>
2022-07-12 10:20:01 -0300

    add session lens

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -30,11 +30,16 @@ return require('packer').startup(function(use)
 
   -- Sessions
   use {
-    'rmagatti/auto-session',
+    'rmagatti/session-lens',
+    requires = {'rmagatti/auto-session', 'nvim-telescope/telescope.nvim'},
     config = function()
       require('auto-session').setup {
         log_level = 'info',
         auto_session_suppression_dirs = { '~/Projects' }
+      }
+      require('session-lens').setup {
+        path_display = {'shorten'},
+        theme_conf = { border = false },
       }
     end
   }
