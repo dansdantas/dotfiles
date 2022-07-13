1ba04c6cc4e459b0480e81cd73bd9c7e6ef13ea3
Daniel Santos Dantas <dansdantas@gmail.com>
2022-07-12 23:32:41 -0300

    add which key

---
+++ .config/nvim/lua/plugins/which_key.lua
@@ -0,0 +1,80 @@
+local tbuiltin = require("telescope.builtin")
+local wk = require("which-key")
+local workspace = require("workspace")
+
+local search_dotfiles = function()
+  require('telescope.builtin').find_files {
+    prompt_title = 'dotfiles',
+    cwd = "$HOME",
+    find_command = {
+      'dot',
+      'ls-tree',
+      '--full-tree',
+      '-r',
+      '--name-only',
+      'HEAD'
+    }
+  }
+end
+
+wk.register({
+  ["<leader>"] = {
+    t = {
+      name = "Telescope",
+      o = { tbuiltin.find_files, "files" },
+      b = { tbuiltin.buffers, "buffers" },
+      d = { search_dotfiles, "dotfiles" },
+      g = { tbuiltin.grep_string, "grep" },
+      ['?'] = { tbuiltin.oldfiles, "recent files" },
+
+      s = {
+        f = { function() return tbuiltin.find_files { previewer = false } end, "files without preview" },
+        g = { tbuiltin.live_grep, "live grep" }
+      }
+    },
+
+    e = { tbuiltin.current_buffer_fuzzy_find, "fuzzy find on buffer" },
+
+
+    b = {
+      name = "buffer",
+      f = { tbuiltin.buffers, "find buffers" },
+      e = { tbuiltin.current_buffer_fuzzy_find, "fuzzy find on buffer" },
+      c = { ":bd!<cr>", "close" },
+      d = { ":bd|e#<cr>", "close all but current" },
+      D = { ":%bd!<cr>", "close all" },
+    },
+
+    g = {
+      name = "Git",
+      c = { tbuiltin.git_commits, "commits" },
+      b = { tbuiltin.git_branches, "branches" },
+      s = { ":Git status<cr>", "status" }
+    },
+
+    v = {
+      name = "Vim",
+      c = { tbuiltin.commands, "commands" },
+      h = { tbuiltin.help_tags, "help" },
+    },
+
+    l = {
+      name = "load",
+      u = { ":luafile %<cr>", "current file as lua" }
+    },
+
+    Q = { ":qa<cr>", "Quit" }
+  },
+
+  [","] = {
+    w = {
+      name = "workspace",
+      a = { workspace.add_dir, "add dir" },
+      r = { workspace.remove_dir, "remove dir" },
+      f = { workspace.find_files, "find files" },
+      g = { workspace.grep_files, "grep on files " },
+      c = { workspace.clean_dirs, "clean dirs" },
+      l = { function() return P(workspace.list_dirs()) end, "list dirs" }
+    },
+  },
+})
