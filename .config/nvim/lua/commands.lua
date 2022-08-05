324daf7c8dae7f52d72ba4133d42316b69a499b6
Daniel Santos Dantas <dansdantas@gmail.com>
2022-08-05 17:25:15 -0300

    clear autocommand for packer plugin update

--- .config/nvim/lua/commands.lua
+++ .config/nvim/lua/commands.lua
@@ -10,7 +10,7 @@ autocommand({ 'BufRead', 'BufNewFile' }, {
   command = 'setfiletype ruby'
 })
 
-local packer_user_config = vim.api.nvim_create_augroup('PackerUserConfig', {})
+local packer_user_config = vim.api.nvim_create_augroup('PackerUserConfig', { clear = true })
 autocommand({ 'BufWritePost' }, {
   pattern = { 'plugins.lua' },
   command = 'source <afile> | PackerCompile',
@@ -21,5 +21,13 @@ autocommand("FileType", {
   pattern = "qf",
   callback = function ()
     vim.opt_local.buflisted = false
+  end
+})
+
+autocommand("BufWritePre", {
+  pattern = "NeogitCommitMessage",
+  callback = function ()
+    print('called')
+    vim.fn.system({"touch", "~/.dotfiles.git/NEOGIT_COMMIT_EDITMSG"})
   end
 })
