ddbb361df674578b8fc70c8a5c404b6714a6fbc4
Daniel Santos Dantas <dansdantas@gmail.com>
2022-08-08 10:42:42 -0300

    more adjustments on bufferline to look like on plugin

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,8 +1,11 @@
+---@diagnostic disable: assign-type-mismatch
 require('bufferline').setup {
   options = {
+    indicator_icon = "",
     diagnostics = 'nvim_lsp',
     mode = 'tabs',
     show_buffer_close_icons = false,
     show_buffer_icons = false,
+    separator_style = { "", "" },
   }
 }
