0513a7ab48550b6458c45ff7a15cbec75b27d34a
Daniel Santos Dantas <dansdantas@gmail.com>
2022-08-22 16:10:20 -0300

    refactor autocommands declarations

--- .config/nvim/lua/commands.lua
+++ .config/nvim/lua/commands.lua
@@ -1,33 +1,33 @@
-local autocommand = vim.api.nvim_create_autocmd
+local au = vim.api.nvim_create_autocmd
+local aug = vim.api.nvim_create_augroup
 
-autocommand("FileType", {
+au("FileType", {
   pattern = "vue",
-  command = 'syntax sync fromstart'
+  command = "syntax sync fromstart"
 })
 
-autocommand({ 'BufRead', 'BufNewFile' }, {
-  pattern = { '*.arb' },
-  command = 'setfiletype ruby'
+au({ "BufRead", "BufNewFile" }, {
+  pattern = { "*.arb" },
+  command = "setfiletype ruby"
 })
 
-local packer_user_config = vim.api.nvim_create_augroup('PackerUserConfig', { clear = true })
-autocommand({ 'BufWritePost' }, {
-  pattern = { 'plugins.lua' },
-  command = 'source <afile> | PackerCompile',
-  group = packer_user_config
+au({ "BufWritePost" }, {
+  group = aug("PackerUserConfig", { clear = true }),
+  pattern = { "plugins.lua" },
+  command = "source <afile> | PackerCompile",
 })
 
-autocommand("FileType", {
+au("FileType", {
   pattern = "qf",
-  callback = function ()
+  callback = function()
     vim.opt_local.buflisted = false
   end
 })
 
-autocommand("BufWritePre", {
+au("BufWritePre", {
   pattern = "NeogitCommitMessage",
-  callback = function ()
-    print('called')
+  callback = function()
+    print("called")
     vim.fn.system({"touch", "~/.dotfiles.git/NEOGIT_COMMIT_EDITMSG"})
   end
 })
