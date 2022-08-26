426b5ce20b093d8793bf3e9c82edea46d91853d3
Daniel Santos Dantas <dansdantas@gmail.com>
2022-08-26 12:40:22 -0300

    update indicator to new format

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,7 +1,9 @@
 ---@diagnostic disable: assign-type-mismatch
 require('bufferline').setup {
   options = {
-    indicator_icon = "",
+    indicator = {
+      icon = "",
+    },
     diagnostics = 'nvim_lsp',
     mode = 'tabs',
     show_buffer_close_icons = false,
