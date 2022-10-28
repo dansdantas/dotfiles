9d25bb6d20e97799ccfb7e85be3bb4639e1144ba
Daniel Santos Dantas <dansdantas@gmail.com>
2022-10-28 14:03:08 -0300

    let packer use default config for window

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -5,15 +5,6 @@ if fn.empty(fn.glob(install_path)) > 0 then
   PackerBootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
 end
 
-local conf = {
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "single" }
-    end,
-  },
-}
-require('packer').init(conf)
-
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
