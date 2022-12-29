601146fb80a2395281b3145d425c854d56123d37
Daniel Santos Dantas <dansdantas@gmail.com>
2022-12-29 18:44:24 -0300

    add packer after bootstrap for first time

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -6,6 +6,7 @@ local PackerBootstrap
 if fn.empty(fn.glob(install_path)) > 0 then
 	PackerBootstrap =
 		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+	vim.cmd([[packadd packer.nvim]])
 end
 
 require("packer").init({
