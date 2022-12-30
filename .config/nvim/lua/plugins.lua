90029b951a6c86ef0f8a8b4e5b3df23f2e5f8c05
Daniel Santos Dantas <dansdantas@gmail.com>
2022-12-30 09:49:54 -0300

    re-enable netrw

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -65,9 +65,6 @@ return require("packer").startup(function(use)
 		opt = true,
 		cmd = { "NvimTreeFindFile", "NvimTreeToggle" },
 		config = function()
-			vim.g.loaded_netrw = 1
-			vim.g.loaded_netrwPlugin = 1
-
 			require("nvim-tree").setup({
 				sort_by = "case_sensitive",
 				view = {
