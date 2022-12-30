6e9419c513eb8f4e6702103909112e7c00cb24f9
Daniel Santos Dantas <dansdantas@gmail.com>
2022-12-30 10:57:46 -0300

    use lsp server configs from utils

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,7 +1,3 @@
-local runtime_path = vim.split(package.path, ";")
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
-
 require("fidget").setup({})
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
@@ -10,71 +6,15 @@ capabilities.textDocument.completion.completionItem.snippetSupport = true
 -- Setup lspconfig.
 local lsp = require("lspconfig")
 
-local servers = {
-	clangd = {},
-	gopls = {
-		cmd = { "gopls" },
-		settings = {
-			gopls = {
-				experimentalPostfixCompletions = true,
-				analyses = {
-					unusedparams = true,
-					shadow = true,
-				},
-				staticcheck = true,
-			},
-		},
-		init_options = {
-			usePlaceholders = true,
-		},
-	},
-	pyright = {},
-	rust_analyzer = {},
-	tsserver = {},
-	vimls = {},
-	solargraph = {
-		autostart = false,
-		settings = {
-			init_options = {
-				formatting = true,
-			},
-			solargraph = {
-				diagnostics = true,
-			},
-		},
-	},
-	sumneko_lua = {
-		settings = {
-			Lua = {
-				runtime = {
-					-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-					version = "LuaJIT",
-					-- Setup your lua path
-					path = runtime_path,
-				},
-				diagnostics = {
-					-- Get the language server to recognize the `vim` global
-					globals = { "vim", "table", "pairs", "package", "require", "ipairs", "pcall" },
-				},
-				workspace = {
-					-- Make the server aware of Neovim runtime files
-					library = vim.api.nvim_get_runtime_file("", true),
-					checkThirdParty = false,
-				},
-				-- Do not send telemetry data containing a randomized but unique identifier
-				telemetry = { enable = false },
-			},
-		},
-	},
-}
-
 local default_conf = {
 	flags = { debounce_text_changes = 150 },
 	capabilities = capabilities,
 	on_attach = require("functions").on_attach_lsp,
 }
 
-for server, conf in pairs(servers) do
+local utils = require("utils")
+
+for server, conf in pairs(utils.lsp_server_configs) do
 	local base_conf = table.copy(default_conf)
 	local full_conf = table.merge(base_conf, conf)
 	lsp[server].setup(full_conf)
