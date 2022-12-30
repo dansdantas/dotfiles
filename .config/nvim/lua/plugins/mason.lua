77afa763ab76361632629ad62f02d2258e6c7eb3
Daniel Santos Dantas <dansdantas@gmail.com>
2022-12-30 10:51:12 -0300

    split mason config from lsp

---
+++ .config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,16 @@
+local utils = require("utils")
+
+require("mason").setup({
+	PATH = "append",
+	ui = {
+		check_outdated_packages_on_open = false,
+	},
+})
+
+require("mason-lspconfig").setup({
+	ensure_installed = utils.lsp_server_names,
+})
+
+require("mason-null-ls").setup({
+	ensure_installed = { "stylua", "eslint_d", "prettierd" },
+})
