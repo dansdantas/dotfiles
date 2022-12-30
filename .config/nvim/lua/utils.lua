18170ad7e6e2e36c58c1d872479521e99d99895e
Daniel Santos Dantas <dansdantas@gmail.com>
2022-12-30 10:50:34 -0300

    move lsp shared config to a utils files

---
+++ .config/nvim/lua/utils.lua
@@ -0,0 +1,74 @@
+local M = {}
+
+local runtime_path = vim.split(package.path, ";")
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+
+M.lsp_server_configs = {
+	clangd = {},
+	gopls = {
+		cmd = { "gopls" },
+		settings = {
+			gopls = {
+				experimentalPostfixCompletions = true,
+				analyses = {
+					unusedparams = true,
+					shadow = true,
+				},
+				staticcheck = true,
+			},
+		},
+		init_options = {
+			usePlaceholders = true,
+		},
+	},
+	pyright = {},
+	rust_analyzer = {},
+	tsserver = {},
+	vimls = {},
+	solargraph = {
+		autostart = false,
+		settings = {
+			init_options = {
+				formatting = true,
+			},
+			solargraph = {
+				diagnostics = true,
+			},
+		},
+	},
+	sumneko_lua = {
+		settings = {
+			Lua = {
+				runtime = {
+					-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+					version = "LuaJIT",
+					-- Setup your lua path
+					path = runtime_path,
+				},
+				diagnostics = {
+					-- Get the language server to recognize the `vim` global
+					globals = { "vim", "table", "pairs", "package", "require", "ipairs", "pcall" },
+				},
+				workspace = {
+					-- Make the server aware of Neovim runtime files
+					library = vim.api.nvim_get_runtime_file("", true),
+					checkThirdParty = false,
+				},
+				-- Do not send telemetry data containing a randomized but unique identifier
+				telemetry = { enable = false },
+			},
+		},
+	},
+}
+
+function M.lsp_server_names()
+	local names = {}
+	for server, _ in pairs(M.lsp_server_configs) do
+		table.insert(names, server)
+	end
+
+	return names
+end
+
+return M
