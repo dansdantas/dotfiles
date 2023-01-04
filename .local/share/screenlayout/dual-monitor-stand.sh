76de817ebc95c15bf5b7b8419ad57cda8d62856f
Daniel Santos Dantas <dansdantas@gmail.com>
2023-01-04 13:07:23 -0300

    fix distance

--- .local/share/screenlayout/dual-monitor-stand.sh
+++ .local/share/screenlayout/dual-monitor-stand.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 xrandr --output HDMI-0 --off
-xrandr --output DP-0 --mode 1920x1080 --pos 2554x0 --rotate left
+xrandr --output DP-0 --mode 1920x1080 --pos 2560x0 --rotate left
 xrandr --output DP-1 --off
 xrandr --output DP-2 --mode 2560x1440 --pos 0x0 --rotate normal
 xrandr --output DP-3 --off
