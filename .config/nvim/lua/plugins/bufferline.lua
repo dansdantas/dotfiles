5ec7c9155441a5bd24e2c0b7dccaee67bc4951e5
Daniel Santos Dantas <dansdantas@gmail.com>
2023-08-27 14:40:58 -0300

    re-add buffer icons and reduce size

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,12 +1,11 @@
----@diagnostic disable: assign-type-mismatch
 require("bufferline").setup({
 	options = {
 		indicator = {
-			icon = "",
+			style = "none",
 		},
+		tab_size = 10,
 		diagnostics = "nvim_lsp",
 		show_buffer_close_icons = false,
-		show_buffer_icons = false,
 		separator_style = { "", "" },
 	},
 })
