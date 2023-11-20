4ac970d7a7fc6bdfd80e36992e5ce5c704d3275a
Daniel Santos Dantas <daniel@sdantas.dev>
2023-11-19 22:11:39 -0300

    rely on previewer config instead of building complex options

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,61 +2,18 @@ local function config()
 	local telescope = require("telescope")
 	local actions = require("telescope.actions")
 	local fb_actions = require("telescope").extensions.file_browser.actions
-	local previewers = require("telescope.previewers")
-	local Job = require("plenary.job")
-
-	local _bad = { ".*%.csv" } -- Put all filetypes that slow you down in this array
-	local bad_files = function(filepath)
-		for _, v in ipairs(_bad) do
-			if filepath:match(v) then
-				return false
-			end
-		end
-
-		return true
-	end
-
-	local new_maker = function(filepath, bufnr, opts)
-		opts = opts or {}
-
-		-- Remove highlighting on specific files
-		if opts.use_ft_detect == nil then
-			opts.use_ft_detect = true
-		end
-		opts.use_ft_detect = opts.use_ft_detect == false and false or bad_files(filepath)
-
-		-- Do not preview on certain file size
-		filepath = vim.fn.expand(filepath)
-		vim.uv.fs_stat(filepath, function(_, stat)
-			if not stat then
-				return
-			end
-			if stat.size > 100000 then
-				return
-			end
-		end)
-
-		Job:new({
-			command = "file",
-			args = { "--mime-type", "-b", filepath },
-			on_exit = function(j)
-				local mime_type = vim.split(j:result()[1], "/")[1]
-				-- If file is binary, do not attempt to preview
-				if mime_type == "text" then
-					previewers.buffer_previewer_maker(filepath, bufnr, opts)
-				else
-					-- maybe we want to write something to the buffer here
-					vim.schedule(function()
-						vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "BINARY" })
-					end)
-				end
-			end,
-		}):sync()
-	end
 
 	telescope.setup({
 		defaults = {
-			buffer_previewer_maker = new_maker,
+			results_title = false,
+			dynamic_preview_title = true,
+
+			preview = {
+				timeout = 400, -- ms
+				filesize_limit = 1, -- Mb
+				ls_short = true, -- ls is only used when displaying directories
+			},
+
 			vimgrep_arguments = {
 				"rg",
 				"--color=never",
@@ -67,10 +24,6 @@ local function config()
 				"--smart-case",
 				"--trim",
 			},
-
-			file_previewer = previewers.vim_buffer_cat.new,
-			grep_previewer = previewers.vim_buffer_vimgrep.new,
-			qflist_previewer = previewers.vim_buffer_qflist.new,
 
 			mappings = {
 				i = {
@@ -92,6 +45,7 @@ local function config()
 				},
 			},
 		},
+
 		extensions = {
 			fzy_native = {
 				override_generic_sorter = false,
@@ -135,15 +89,17 @@ local function config()
 end
 
 return {
-	"nvim-telescope/telescope.nvim",
-	config = config,
-	dependencies = {
-		{ "nvim-lua/plenary.nvim" },
-		{ "nvim-telescope/telescope-ui-select.nvim" },
-		{ "nvim-telescope/telescope-file-browser.nvim" },
-		{
-			"nvim-telescope/telescope-fzf-native.nvim",
-			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+	{
+		"nvim-telescope/telescope.nvim",
+		config = config,
+		dependencies = {
+			{ "nvim-lua/plenary.nvim" },
+			{ "nvim-telescope/telescope-ui-select.nvim" },
+			{ "nvim-telescope/telescope-file-browser.nvim" },
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+			},
 		},
 	},
 }
