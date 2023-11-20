67acc1227891526acb8d06acc9662a56e9092454
Daniel Santos Dantas <daniel@sdantas.dev>
2023-11-20 13:10:49 -0300

    move git plugins config to file

--- .config/nvim/lua/plugins/all.lua
+++ .config/nvim/lua/plugins/all.lua
@@ -142,50 +142,6 @@ return {
 		end,
 	},
 
-	-- Git
-	"tpope/vim-fugitive",
-	"tpope/vim-rhubarb",
-	"tommcdo/vim-fubitive", -- Bitbucket support on fugitive GBrowse
-
-	{
-		"lewis6991/gitsigns.nvim",
-		opts = {
-			signs = {
-				add = { text = "+" },
-				change = { text = "~" },
-				delete = { text = "_" },
-				topdelete = { text = "‾" },
-				changedelete = { text = "~" },
-			},
-		},
-	},
-
-	{
-		"TimUntersberger/neogit",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"sindrets/diffview.nvim",
-		},
-		module = true,
-		config = function()
-			require("neogit").setup({
-				integrations = {
-					diffview = true,
-				},
-			})
-			require("diffview").setup({
-				hooks = {
-					diff_buf_read = function()
-						-- Change local options in diff buffers
-						vim.opt_local.wrap = false
-						vim.opt_local.list = false
-						vim.opt_local.colorcolumn = { 80 }
-					end,
-				},
-			})
-		end,
-	},
-
 	"szw/vim-maximizer",
 
 	-- Languages
