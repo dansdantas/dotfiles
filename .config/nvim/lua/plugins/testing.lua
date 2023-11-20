172aff751ae21ded5b61e608a063f6d8055fca48
Daniel Santos Dantas <daniel@sdantas.dev>
2023-11-20 15:06:29 -0300

    move globals definition into plugin initialization

--- .config/nvim/lua/plugins/testing.lua
+++ .config/nvim/lua/plugins/testing.lua
@@ -1,13 +1,16 @@
-local g = vim.g
-
--- Test
-g["test#strategy"] = "floaterm"
-g["test#preserve_screen"] = 1
-g["test#echo_command"] = 0
-g["test#neovim#term_position"] = "vert botright 50"
-
 return {
-	"janko-m/vim-test",
+	{
+		"janko-m/vim-test",
+		init = function()
+			local g = vim.g
+
+			-- Test
+			g["test#strategy"] = "floaterm"
+			g["test#preserve_screen"] = 1
+			g["test#echo_command"] = 0
+			g["test#neovim#term_position"] = "vert botright 50"
+		end,
+	},
 	"benmills/vimux",
 	"voldikss/vim-floaterm",
 
