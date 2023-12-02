e100d830c28b4532d10f38b1c645174fd0826fbe
Daniel Santos Dantas <daniel@sdantas.dev>
2023-12-02 15:09:02 -0300

    highlight urls

--- .config/nvim/lua/plugins/layout.lua
+++ .config/nvim/lua/plugins/layout.lua
@@ -63,8 +63,26 @@ return {
 
 	{
 		"rcarriga/nvim-notify",
-		config = function ()
-			vim.notify = require('notify')
-		end
-	}
+		config = function()
+			vim.notify = require("notify")
+		end,
+	},
+
+	{
+		"itchyny/vim-highlighturl",
+		event = "VeryLazy",
+		config = function()
+			-- Disable the plugin in some places where the default highlighting
+			-- is preferred.
+			vim.api.nvim_create_autocmd("FileType", {
+				desc = "Disable URL highlights",
+				pattern = {
+					"fzf",
+					"lazyterm",
+					"spectre_panel",
+				},
+				command = "call highlighturl#disable_local()",
+			})
+		end,
+	},
 }
