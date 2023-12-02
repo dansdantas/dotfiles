d894f6abeee1a39b1c481d38e8a6ffd3fc0bd2f0
Daniel Santos Dantas <daniel@sdantas.dev>
2023-12-02 14:25:58 -0300

    move plugin definition into opts

--- .config/nvim/lua/plugins/sessions.lua
+++ .config/nvim/lua/plugins/sessions.lua
@@ -2,16 +2,19 @@
 return {
 	{
 		"rmagatti/session-lens",
-		dependencies = { "rmagatti/auto-session", "nvim-telescope/telescope.nvim" },
-		config = function()
-			require("auto-session").setup({
-				log_level = "info",
-				auto_session_suppression_dirs = { "~/Projects" },
-			})
-			require("session-lens").setup({
-				path_display = { "shorten" },
-				theme_conf = { border = false },
-			})
-		end,
+		opts = {
+			path_display = { "shorten" },
+			theme_conf = { border = false },
+		},
+		dependencies = {
+			{
+				"rmagatti/auto-session",
+				opts = {
+					log_level = "error",
+					auto_session_suppression_dirs = { "~/Projects" },
+				},
+			},
+			"nvim-telescope/telescope.nvim",
+		},
 	},
 }
