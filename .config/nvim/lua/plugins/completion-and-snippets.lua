1a534e6bb67c5e37f5f47c87e64b81cb6745140a
Daniel Santos Dantas <daniel@sdantas.dev>
2023-12-04 11:09:18 -0300

    move snippets config inside opts, config properties

--- .config/nvim/lua/plugins/completion-and-snippets.lua
+++ .config/nvim/lua/plugins/completion-and-snippets.lua
@@ -38,11 +38,13 @@ local function config()
 		mapping = cmp.mapping.preset.insert({
 			["<C-b>"] = cmp.mapping.scroll_docs(-4),
 			["<C-f>"] = cmp.mapping.scroll_docs(4),
+
 			---@diagnostic disable-next-line: missing-parameter
 			["<C-y>"] = { i = cmp.mapping.confirm({ select = false }) },
 			["<C-n>"] = { i = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }) },
 			["<C-p>"] = { i = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }) },
 			["<C-e>"] = { i = cmp.mapping.abort() },
+			["/"] = { i = cmp.mapping.close() },
 
 			-- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 			["<CR>"] = cmp.mapping({
@@ -126,30 +128,6 @@ local function config()
 		},
 	})
 
-	vim.keymap.set({ "i", "s" }, "<C-k>", function()
-		if luasnip.jumpable(-1) then
-			luasnip.jump(-1)
-		end
-	end, { silent = true })
-
-	vim.keymap.set({ "i", "s" }, "<C-space>", function()
-		if luasnip.expand_or_locally_jumpable() then
-			luasnip.expand_or_jump()
-		end
-	end, { silent = true })
-
-	vim.keymap.set({ "i", "s" }, "<C-j>", function()
-		if luasnip.locally_jumpable(1) then
-			luasnip.locally_jump(1)
-		end
-	end, { silent = true })
-
-	vim.keymap.set("i", "<C-l>", function()
-		if luasnip.choice_active() then
-			luasnip.change_choice(1)
-		end
-	end, { silent = true })
-
 	-- `/` cmdline setup.
 	cmp.setup.cmdline("/", {
 		mapping = cmp.mapping.preset.cmdline(),
@@ -172,23 +150,6 @@ local function config()
 		}),
 	})
 
-	require("luasnip").config.set_config({
-		history = true,
-	})
-
-	-- DOCS https://github.com/L3MON4D3/LuaSnip/blob/master/DOC.md#api-reference
-	require("luasnip").setup({
-		region_check_events = "CursorMoved", -- prevent <Tab> jumping back to a snippet after it has been left early
-		update_events = { "TextChanged", "TextChangedI" }, -- live updating of snippets
-	})
-
-	require("luasnip").filetype_extend("ruby", { "rails" })
-
-	-- VS-code-style snippets
-	require("luasnip.loaders.from_vscode").lazy_load()
-	require("luasnip.loaders.from_snipmate").lazy_load()
-	require("luasnip.loaders.from_lua").lazy_load()
-
 	local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 	cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 end
@@ -204,13 +165,12 @@ return {
 			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-cmdline",
 			"hrsh7th/cmp-nvim-lua",
+
 			"onsails/lspkind.nvim",
 			"amarakon/nvim-cmp-buffer-lines",
 			"lukas-reineke/cmp-under-comparator",
 
-			{ "L3MON4D3/LuaSnip", event = "InsertEnter" }, -- snippet engine
 			"saadparwaiz1/cmp_luasnip", -- adapter for snippet engine
-			"honza/vim-snippets", -- more snippets
 
 			-- "tamago324/cmp-zsh", -- some shell completions
 			-- "dmitmel/cmp-cmdline-history",
@@ -219,6 +179,113 @@ return {
 				opts = {
 					disable_filetype = { "TelescopePrompt", "vim" },
 				},
+			},
+
+			{ -- snippet engine
+				"L3MON4D3/LuaSnip",
+				event = "InsertEnter",
+				dependencies = {
+					"honza/vim-snippets", -- more snippets
+				},
+				opts = function()
+					local types = require("luasnip.util.types")
+
+					-- DOCS https://github.com/L3MON4D3/LuaSnip/blob/master/DOC.md#api-reference
+					return {
+						region_check_events = "CursorMoved", -- prevent <Tab> jumping back to a snippet after it has been left early
+						update_events = { "TextChanged", "TextChangedI" }, -- live updating of snippets
+						ext_opts = {
+							[types.insertNode] = {
+								unvisited = {
+									virt_text = { { "|", "Conceal" } },
+									virt_text_pos = "inline",
+								},
+							},
+							[types.exitNode] = {
+								unvisited = {
+									virt_text = { { "|", "Conceal" } },
+									virt_text_pos = "inline",
+								},
+							},
+						},
+						snip_env = {
+							-- Helper function for showing a snippet if the Treesitter node
+							-- satisfies a given predicate.
+							ts_show = function(pred)
+								return function()
+									local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+									local ok, node = pcall(vim.treesitter.get_node, { pos = { row - 1, col - 1 } })
+
+									-- Show the snippet if Treesitter bails.
+									if not ok or not node then
+										return true
+									end
+
+									return pred(node:type())
+								end
+							end,
+						},
+					}
+				end,
+				config = function(_, opts)
+					local luasnip = require("luasnip")
+					luasnip.setup(opts)
+
+					vim.keymap.set({ "i", "s" }, "<C-space>", function()
+						if luasnip.expand_or_locally_jumpable() then
+							luasnip.expand_or_jump()
+						end
+					end, { silent = true })
+
+					vim.keymap.set({ "i", "s" }, "<C-k>", function()
+						if luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						end
+					end, { silent = true })
+
+					vim.keymap.set({ "i", "s" }, "<C-j>", function()
+						if luasnip.locally_jumpable(1) then
+							luasnip.locally_jump(1)
+						end
+					end, { silent = true })
+
+					-- Use <C-c> to select a choice in a snippet.
+					vim.keymap.set({ "i", "s" }, "<C-c>", function()
+						if luasnip.choice_active() then
+							require("luasnip.extras.select_choice")()
+						end
+					end, { desc = "Select choice" })
+
+					vim.keymap.set("i", "<C-l>", function()
+						if luasnip.choice_active() then
+							luasnip.change_choice(1)
+						end
+					end, { silent = true })
+
+					-- extend ruby snippets to rails
+					require("luasnip").filetype_extend("ruby", { "rails" })
+
+					-- VS-code-style snippets
+					require("luasnip.loaders.from_vscode").lazy_load()
+					require("luasnip.loaders.from_snipmate").lazy_load()
+					require("luasnip.loaders.from_lua").lazy_load()
+
+					vim.api.nvim_create_autocmd("ModeChanged", {
+						group = vim.api.nvim_create_augroup("personal/unlink_snippet", { clear = true }),
+						desc = "Cancel the snippet session when leaving insert mode",
+						pattern = { "s:n", "i:*" },
+						callback = function(args)
+							if
+								luasnip.session
+								and luasnip.session.current_nodes[args.buf]
+								and not luasnip.session.jump_active
+								and not luasnip.choice_active()
+							then
+								luasnip.unlink_current()
+							end
+						end,
+					})
+				end,
 			},
 		},
 	},
