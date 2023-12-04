071a3fd482f16ea95da50b548b3875b0201cec47
Daniel Santos Dantas <daniel@sdantas.dev>
2023-12-03 23:36:45 -0300

    move dap configuration inside lazy scope

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -1,72 +1,79 @@
 --# selene: allow(mixed_table) -- lazy.nvim uses them
-local function config()
-	local dap = require("dap")
-
-	vim.keymap.set("n", ",do", require("dapui").open, { desc = "[D]ap open" })
-	vim.keymap.set("n", ",dx", require("dapui").close, { desc = "[D]ap open" })
-	vim.keymap.set("n", ",dc", dap.continue, { desc = "[D]ap [C]ontinue" })
-	vim.keymap.set("n", ",dp", dap.step_into, { desc = "[D]ap step over" })
-	vim.keymap.set("n", ",dn", dap.step_over, { desc = "[D]ap step into" })
-	vim.keymap.set("n", ",dO", dap.step_out, { desc = "[D]ap step out" })
-	vim.keymap.set("n", ",dt", dap.toggle_breakpoint, { desc = "[D]ap [T]oggle breakpoint" })
-	vim.keymap.set("n", ",de", require("dap-go").debug_test, { desc = "[D]ap D[e]bug" })
-
-	vim.keymap.set("n", ",db", function()
-		return dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
-	end, { desc = "[D]ap set breakpoint" })
-
-	vim.keymap.set("n", ",dl", function()
-		dap.set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
-	end, { desc = "[D]ap set [l]og point message" })
-
-	require("nvim-dap-virtual-text").setup({})
-	require("dap-go").setup({})
-	require("dapui").setup({
-		expand_lines = true,
-		layouts = {
-			{
-				elements = {
-					-- Elements can be strings or table with id and size keys.
-					{ id = "scopes", size = 0.25 },
-					"breakpoints",
-					"stacks",
-					"watches",
-				},
-				size = 40, -- 40 columns
-				position = "left",
-			},
-			{
-				elements = {
-					"repl",
-					"console",
-				},
-				size = 0.25, -- 25% of total lines
-				position = "bottom",
-			},
-		},
-	})
-
-	local dapui = require("dapui")
-	dap.listeners.after.event_initialized["dapui_config"] = function()
-		dapui.open({})
-	end
-
-	dap.listeners.before.event_terminated["dapui_config"] = function()
-		dapui.close({})
-	end
-
-	dap.listeners.before.event_exited["dapui_config"] = function()
-		dapui.close({})
-	end
-end
-
 return {
 	"mfussenegger/nvim-dap",
-	config = config,
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
+
+		-- Automatically open the UI when a new debug session is created.
+		dap.listeners.after.event_initialized["dapui_config"] = function()
+			dapui.open({})
+		end
+		dap.listeners.before.event_terminated["dapui_config"] = function()
+			dapui.close({})
+		end
+		dap.listeners.before.event_exited["dapui_config"] = function()
+			dapui.close({})
+		end
+	end,
+	keys = {
+		-- stylua: ignore start
+		{ ",do", function() require("dapui").open() end, desc = "[D]ap open" },
+		{ ",dx", function() require("dapui").close() end, desc = "[D]ap open" },
+		{ ",dc", function() require("dap").continue() end, desc = "[D]ap [C]ontinue"},
+		{ ",dp", function() require("dap").step_into() end, desc = "[D]ap step over" },
+		{ ",dn", function() require("dap").step_over() end, desc = "[D]ap step into" },
+		{ ",dO", function() require("dap").step_out() end, desc = "[D]ap step out" },
+		{ ",dt", function() require("dap").toggle_breakpoint() end, desc = "[D]ap [T]oggle breakpoint" },
+		{ ",de", function() require("dap-go").debug_test() end, desc = "[D]ap D[e]bug" },
+		-- stylua: ignore end
+
+		{
+			",db",
+			function()
+				require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+			end,
+			desc = "[D]ap set breakpoint",
+		},
+
+		{
+			",dl",
+			function()
+				require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+			end,
+			desc = "[D]ap set [l]og point message",
+		},
+	},
 	dependencies = {
-		"rcarriga/nvim-dap-ui",
-		"theHamsta/nvim-dap-virtual-text",
 		"nvim-telescope/telescope-dap.nvim",
-		"leoluz/nvim-dap-go",
+		{ "theHamsta/nvim-dap-virtual-text", opts = { virt_text_pos = "eol" } },
+		{ "leoluz/nvim-dap-go", opts = {} },
+		{
+			"rcarriga/nvim-dap-ui",
+			opts = {
+				expand_lines = true,
+				layouts = {
+					{
+						elements = {
+							-- Elements can be strings or table with id and size keys.
+							{ id = "scopes", size = 0.25 },
+							"breakpoints",
+							"stacks",
+							"watches",
+						},
+						size = 40, -- 40 columns
+						position = "left",
+					},
+					{
+						elements = {
+							"repl",
+							"console",
+						},
+						size = 0.25, -- 25% of total lines
+						position = "bottom",
+					},
+				},
+			},
+		},
 	},
 }
