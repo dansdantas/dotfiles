6932d1aad715736d774c18f20c6077138fe291ef
Daniel Santos Dantas <daniel@sdantas.dev>
2023-12-05 13:10:00 -0300

    add autocommand to check docker compose

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -77,3 +77,38 @@ au({ "BufNew", "BufReadPost" }, {
 		end, 1)
 	end,
 })
+
+--------------------------------------------------------------------------------
+-- Docker compose tests
+
+-- Define a flag to check if the autocommand has already run
+local has_run_once = false
+
+-- Define the function to check for the presence of docker-compose.yml
+local function check_for_docker_compose()
+	if not has_run_once then
+		local root_dir = vim.fn.getcwd()
+
+		local docker_compose_exists = vim.fn.filereadable(root_dir .. "/docker-compose.yml") == 1
+
+		if docker_compose_exists then
+			vim.g["test#ruby#rails#executable"] = "docker compose run -e RAILS_ENV=test app bundle exec rake test"
+			vim.g["test#ruby#rspec#executable"] = "docker compose run -e RAILS_ENV=test app bundle exec rspec"
+		end
+
+		-- Set the flag to true to indicate that the autocommand has run
+		has_run_once = true
+	end
+end
+
+au({ "VimEnter", "BufEnter", "BufNewFile" }, {
+	pattern = { "*.rb" },
+	callback = check_for_docker_compose,
+})
+
+au({ "DirChanged" }, {
+	pattern = { "*" },
+	callback = function()
+		has_run_once = false
+	end,
+})
