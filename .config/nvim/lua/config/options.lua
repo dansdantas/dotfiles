504e5cb59620bc20cb3fb4bb96327e19fd0249fe
Daniel Santos Dantas <daniel@sdantas.dev>
2023-12-16 07:50:53 -0300

    update python 3 version

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -142,7 +142,7 @@ local env = vim.env
 
 -- Python
 g.python_host_prog = env.PYENV_ROOT .. "/versions/2.7.18/bin/python"
-g.python3_host_prog = env.PYENV_ROOT .. "/versions/3.11.1/bin/python"
+g.python3_host_prog = env.PYENV_ROOT .. "/versions/3.12.0/bin/python"
 
 -- Ruby
 g.loaded_ruby_provider = 0
