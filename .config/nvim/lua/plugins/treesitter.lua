3b6cea018602c35e9311c89a1db167f17a61a23f
Daniel Santos Dantas <daniel@sdantas.dev>
2023-12-18 10:01:25 -0300

    add commentary for some treesitter-txtobjects changes, modify gx to search next string and add script to be able to delete surround indentation

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -64,6 +64,28 @@ local options = {
 				["ac"] = "@class.outer",
 				["ic"] = "@class.inner",
 			},
+			-- You can choose the select mode (default is charwise 'v')
+			--
+			-- Can also be a function which gets passed a table with the keys
+			-- * query_string: eg '@function.inner'
+			-- * method: eg 'v' or 'o'
+			-- and should return the mode ('v', 'V', or '<c-v>') or a table
+			-- mapping query_strings to modes.
+			selection_modes = {
+				["@parameter.outer"] = "v", -- charwise
+				["@function.outer"] = "V", -- linewise
+				["@class.outer"] = "<c-v>", -- blockwise
+			},
+			-- If you set this to `true` (default is `false`) then any textobject is
+			-- extended to include preceding or succeeding whitespace. Succeeding
+			-- whitespace has priority in order to act similarly to eg the built-in
+			-- `ap`.
+			--
+			-- Can also be a function which gets passed a table with the keys
+			-- * query_string: eg '@function.inner'
+			-- * selection_mode: eg 'v'
+			-- and should return true of false
+			include_surrounding_whitespace = true,
 		},
 		move = {
 			enable = true,
@@ -148,6 +170,61 @@ return {
 		opts = {
 			useDefaultKeymaps = true,
 			disabledKeymaps = { "gc", "in", "an", "il", "al" },
+		},
+		keys = {
+			{
+				"gx",
+				function()
+					-- select URL
+					require("various-textobjs").url()
+
+					-- plugin only switches to visual mode when textobj found
+					local foundURL = vim.fn.mode():find("v")
+					if not foundURL then
+						return
+					end
+
+					-- retrieve URL with the z-register as intermediary
+					vim.cmd.normal({ '"zy', bang = true })
+					local url = vim.fn.getreg("z")
+
+					-- open with the OS-specific shell command
+					local opener
+					if vim.fn.has("macunix") == 1 then
+						opener = "open"
+					elseif vim.fn.has("linux") == 1 then
+						opener = "xdg-open"
+					elseif vim.fn.has("win64") == 1 or vim.fn.has("win32") == 1 then
+						opener = "start"
+					end
+					local openCommand = string.format("%s '%s' >/dev/null 2>&1", opener, url)
+					vim.fn.system(openCommand)
+				end,
+				desc = "URL opener",
+			},
+			{
+				"dsi",
+				function()
+					require("various-textobjs").indentation("outer", "outer")
+
+					-- plugin only switches to visual mode when a textobj has been found
+					local indentationFound = vim.fn.mode():find("V")
+					if not indentationFound then
+						return
+					end
+
+					-- dedent indentation
+					vim.cmd.normal({ "<", bang = true })
+
+					-- delete surrounding lines
+					local endBorderLn = vim.api.nvim_buf_get_mark(0, ">")[1]
+					local startBorderLn = vim.api.nvim_buf_get_mark(0, "<")[1]
+
+					vim.cmd(tostring(endBorderLn) .. " delete") -- delete end first so line index is not shifted
+					vim.cmd(tostring(startBorderLn) .. " delete")
+				end,
+				desc = "Delete surrounding indentation",
+			},
 		},
 	},
 
