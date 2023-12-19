f224a531daf2f38b561f3eacc049a23b934ad1af
Daniel Santos Dantas <daniel@sdantas.dev>
2023-12-19 08:03:24 -0300

    convert url to string and use vim.ui.open instead of calculating opener

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -194,19 +194,10 @@ return {
 
 					-- retrieve URL with the z-register as intermediary
 					vim.cmd.normal({ '"zy', bang = true })
-					local url = vim.fn.getreg("z")
+					local url = tostring(vim.fn.getreg("z"))
 
 					-- open with the OS-specific shell command
-					local opener
-					if vim.fn.has("macunix") == 1 then
-						opener = "open"
-					elseif vim.fn.has("linux") == 1 then
-						opener = "xdg-open"
-					elseif vim.fn.has("win64") == 1 or vim.fn.has("win32") == 1 then
-						opener = "start"
-					end
-					local openCommand = string.format("%s '%s' >/dev/null 2>&1", opener, url)
-					vim.fn.system(openCommand)
+					vim.ui.open(url)
 				end,
 				desc = "URL opener",
 			},
