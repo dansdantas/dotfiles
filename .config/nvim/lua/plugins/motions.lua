38a91afd770b778907f1cac3f1897d09f1d9cee4
Daniel Santos Dantas <daniel@sdantas.dev>
2023-12-20 20:44:31 -0300

    replace leap with flash.nvim

--- .config/nvim/lua/plugins/motions.lua
+++ .config/nvim/lua/plugins/motions.lua
@@ -12,16 +12,36 @@ return {
 	"wellle/targets.vim",
 
 	{
-		"ggandor/leap.nvim",
-		config = function()
-			require("leap").set_default_keymaps()
-		end,
-	},
-
-	{
 		"szw/vim-maximizer",
 		keys = {
 			{ "<leader>mt", ":MaximizerToggle<cr>", desc = "toggle maximize on current buffer" },
+		},
+	},
+
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		opts = {
+			search = {
+				mode = "fuzzy",
+				incremental = true,
+			},
+			modes = {
+				char = { enabled = false },
+				search = { enabled = false },
+			},
+			prompt = {
+				enabled = false,
+			},
+		},
+		keys = {
+			-- stylua: ignore start
+			{ "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash search", },
+			{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash", },
+			-- { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter", },
+			-- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash", },
+			-- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search", },
+			-- stylua: ignore end
 		},
 	},
 }
