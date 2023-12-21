d3fa0a084ce113ebddbba6e03ca4940c9cd24d76
Daniel Santos Dantas <daniel@sdantas.dev>
2023-12-21 13:34:24 -0300

    move which-key to other plugin patterns

--- .config/nvim/lua/plugins/which_key.lua
+++ .config/nvim/lua/plugins/which_key.lua
@@ -1,27 +1,24 @@
 --# selene: allow(mixed_table) -- lazy.nvim uses them
-local function config()
-	-- plugins
-	local wk = require("which-key")
-
-	wk.setup({})
-
-	-- Leader mappings
-	wk.register({
-		g = { name = "git" },
-		t = { name = "telescope" },
-		b = { name = "buffer" },
-	}, { prefix = "<leader>" })
-
-	-- Only for declaration purposes
-	wk.register({
-		d = { name = "dap" },
-		g = { name = "git" },
-		t = { name = "testing" },
-		w = { name = "workspace" },
-	}, { prefix = "," })
-end
-
 return {
 	"folke/which-key.nvim",
-	config = config,
+	opts = {},
+	config = function(_, opts)
+		local wk = require("which-key")
+		wk.setup(opts)
+
+		-- Leader mappings
+		wk.register({
+			g = { name = "git" },
+			t = { name = "telescope" },
+			b = { name = "buffer" },
+		}, { prefix = "<leader>" })
+
+		-- Only for declaration purposes
+		wk.register({
+			d = { name = "dap" },
+			g = { name = "git" },
+			t = { name = "testing" },
+			w = { name = "workspace" },
+		}, { prefix = "," })
+	end,
 }
