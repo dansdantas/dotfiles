ae7c99c7be74952f3e44ef16dbea844fd2c79288
Daniel Santos Dantas <daniel@sdantas.dev>
2024-02-14 10:11:00 -0300

    bufferline config changes
      * use uppercase to move into tabs
      * move buffers with "bj, bk"
      * re-add separator between buffers
      * add offset on file tree
      * remove minimal size for buffer

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -7,9 +7,11 @@ return {
 		keys = {
 			-- stylua: ignore start
 			{ "<leader>j", function() require("bufferline").cycle(-1) end, desc = "previous buffer" },
-			{ "<leader>J", function() require("bufferline").move(-1) end, desc = "move buffer backwards" },
 			{ "<leader>k", function() require("bufferline").cycle(1) end, desc = "next buffer" },
-			{ "<leader>K", function() require("bufferline").move(1) end, desc = "move buffer forwards" },
+			{ "<leader>J", vim.cmd.tabprev, desc = "prev tab" },
+			{ "<leader>K", vim.cmd.tabnext, desc = "next tab" },
+			{ "<leader>bj", function() require("bufferline").move(-1) end, desc = "move buffer backwards" },
+			{ "<leader>bk", function() require("bufferline").move(1) end, desc = "move buffer forwards" },
 			{ "<leader>bf", function() require("bufferline").pick_buffer() end, desc = "select buffer" },
 			-- stylua: ignore end
 		},
@@ -18,10 +20,18 @@ return {
 				indicator = {
 					style = "none",
 				},
-				tab_size = 10,
+				tab_size = 0,
 				diagnostics = "nvim_lsp",
 				show_buffer_close_icons = false,
-				separator_style = { "", "" },
+				separator_style = "thin",
+				offsets = {
+					{
+						filetype = "NvimTree",
+						text = "Tree",
+						text_align = "center",
+						separator = false,
+					},
+				},
 			},
 		},
 	},
