8907bab2fc787bd6d0765cbcaf5cc495fac7d04f
Daniel Santos Dantas <daniel@sdantas.dev>
2024-05-23 18:53:59 -0300

    remove selene config

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -1,7 +1,4 @@
---# selene: allow(mixed_table) -- lazy.nvim uses them
 return {
 	"windwp/nvim-autopairs",
-	config = function()
-		require("config.autopairs")
-	end,
+	config = function() require("config.autopairs") end,
 }
