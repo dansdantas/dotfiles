01d3cf27e0e3d79259a5362447ec75175a7f8e37
Daniel Santos Dantas <daniel@sdantas.dev>
2024-05-23 10:15:06 -0300

    disable noice.nvim for now

--- .config/nvim/lua/plugins/layout.lua
+++ .config/nvim/lua/plugins/layout.lua
@@ -1,32 +1,6 @@
 --# selene: allow(mixed_table) -- lazy.nvim uses them
 return {
 	{
-		"folke/noice.nvim",
-		event = "VeryLazy",
-		opts = {
-			messages = {
-				enabled = false,
-			},
-			lsp = {
-				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-				override = {
-					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-					["vim.lsp.util.stylize_markdown"] = true,
-					["cmp.entry.get_documentation"] = true,
-				},
-			},
-		},
-		dependencies = {
-			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-			"MunifTanjim/nui.nvim",
-			-- OPTIONAL:
-			--   `nvim-notify` is only needed, if you want to use the notification view.
-			--   If not available, we use `mini` as the fallback
-			"rcarriga/nvim-notify",
-		},
-	},
-
-	{
 		"itchyny/vim-highlighturl",
 		event = "VeryLazy",
 		config = function()
