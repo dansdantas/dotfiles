8ce4fa9b00de542bf38bd044a4f87beec0b512a9
Daniel Santos Dantas <daniel@sdantas.dev>
2024-05-23 22:07:56 -0300

    split git config into file

---
+++ .config/nvim/lua/config/git.lua
@@ -0,0 +1,55 @@
+local gs = require("gitsigns")
+gs.setup({
+	numhl = true,
+	signs = {
+		add = { text = "+" },
+		change = { text = "~" },
+		delete = { text = "_" },
+		topdelete = { text = "‾" },
+		changedelete = { text = "~" },
+	},
+})
+
+-- keymaps
+local set = vim.keymap.set
+
+set("n", "]g", function() gs.next_hunk({ navigation_message = false }) end, { desc = "Git: next hunk" })
+set("n", "[g", function() gs.prev_hunk({ navigation_message = false }) end, { desc = "Git: previous hunk" })
+
+set("n", "<leader>ga", function() gs.stage_hunk() end, { desc = "Git: add hunk" })
+set("n", "<leader>gA", function() gs.stage_buffer() end, { desc = "Git: stage buffer" })
+set("n", "<leader>gu", function() gs.undo_stage_hunk() end, { desc = "Git: unstage hunk" })
+set("n", "<leader>gU", function() gs.reset_buffer_index() end, { desc = "Git: unstage buffer" })
+set("n", "<leader>gR", function() gs.reset_buffer() end, { desc = "Git: reset buffer" })
+set("n", "<leader>gr", function() gs.reset_hunk() end, { desc = "Git: reset hunk" })
+
+set({ "o", "x" }, "ih", function() gs.select_hunk() end, { desc = "Git: hunk" })
+set("n", ",gh", function() gs.select_hunk() end, { desc = "Git: select hunk" })
+
+set("n", "<leader>gp", function() gs.preview_hunk() end, { desc = "Git: preview hunk" })
+set("n", "<leader>gd", function() gs.diffthis() end, { desc = "Git: diff file" })
+set("n", "<leader>gt", function() gs.toggle_deleted() end, { desc = "Git: toggle deleted hunks" })
+
+set("n", "<leader>gq", function() gs.setqflist() end, { desc = "Git: populate hunks to loclist" })
+set("n", "<leader>gb", function() gs.blame_line() end, { desc = "Git: blame line" })
+set("n", "<leader>gB", function() gs.blame_line({ full = true }) end, { desc = "Git: blame full" })
+
+set("n", "<leader>gs", function()
+	local opts = {}
+
+	-- Env variables GIT_WORK_TREE and GIT_DIR solves problems with not finding git still need conditional
+	-- because of expanding files with -u tries to read every file on $HOME
+	if vim.env.GIT_WORK_TREE == vim.env.HOME then
+		opts.expand_dir = false
+	end
+
+	require("telescope.builtin").git_status(opts)
+end, { desc = "Git: status" })
+
+set("n", "<leader>gc", ":Git commit<cr>", { desc = "Git: commit" })
+set("n", "<leader>g!", ":Git commit --amend<cr>", { desc = "Git: commit with amend" })
+
+local tg = require("tinygit")
+set("n", "<leader>gC", function() tg.smartCommit() end, { desc = "Git: commit" })
+set("n", ",gp", function() tg.push() end, { desc = "Git: push" })
+set("n", ",ga", function() tg.amendOnlyMsg() end, { desc = "Git: amend only message" })
--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,4 +1,3 @@
---# selene: allow(mixed_table) -- lazy.nvim uses them
 return {
 	-- Git
 	"tpope/vim-fugitive",
@@ -10,67 +9,11 @@ return {
 			"stevearc/dressing.nvim",
 			"rcarriga/nvim-notify", -- optional, but recommended
 		},
-		keys = {
-			-- stylua: ignore start
-			{ "<leader>gC", function() require("tinygit").smartCommit() end, desc = "Git: commit" },
-			{ ",gp", function() require("tinygit").push() end, desc = "Git: push" },
-			{ ",ga", function() require("tinygit").amendOnlyMsg() end, desc = "Git: commit with amend" },
-			-- stylua: ignore end
-		},
 	},
 
 	{
 		"lewis6991/gitsigns.nvim",
-		lazy = false,
-		opts = {
-			numhl = true,
-			signs = {
-				add = { text = "+" },
-				change = { text = "~" },
-				delete = { text = "_" },
-				topdelete = { text = "‾" },
-				changedelete = { text = "~" },
-			},
-		},
-		keys = {
-			-- stylua: ignore start
-			{ "]g", function() require("gitsigns").next_hunk({navigation_message = false, wrap = false}) end, desc = "Git: next hunk" },
-			{ "[g", function() require("gitsigns").prev_hunk({navigation_message = false, wrap = false}) end, desc = "Git: previous hunk" },
-			{ "<leader>gn", function() require("gitsigns").next_hunk({navigation_message = false}) end, desc = "Git: next hunk" },
-			{ "<leader>gp", function() require("gitsigns").prev_hunk({navigation_message = false}) end, desc = "Git: previous hunk" },
-			{ "<leader>ga", function() require("gitsigns").stage_hunk() end, desc = "Git: add hunk" },
-			{ "<leader>gA", function() require("gitsigns").stage_buffer() end, desc = "Git: stage buffer" },
-			{ "<leader>gu", function() require("gitsigns").undo_stage_hunk() end, desc = "Git: unstage hunk" },
-			{ "<leader>gU", function() require("gitsigns").reset_buffer_index() end, desc = "Git: unstage buffer" },
-			{ "<leader>gP", function() require("gitsigns").preview_hunk() end, desc = "Git: preview hunk" },
-			{ ",gh", function() require("gitsigns").select_hunk() end, desc = "Git: select hunk" },
-			{ "ih", function() require("gitsigns").select_hunk() end, mode = { "o", "x" }, desc = "Git: hunk" },
-			{ "<leader>gd", function() require("gitsigns").diffthis() end, desc = "Git: diff file" },
-			{ "<leader>gq", function() require("gitsigns").setqflist() end, desc = "Git: populate hunks to loclist" },
-			{ "<leader>gt", function() require("gitsigns").toggle_deleted() end, desc = "Git: toggle deleted hunks" },
-			{ "<leader>gR", function() require("gitsigns").reset_buffer() end, desc = "Git: reset buffer" },
-			{ "<leader>gr", function() require("gitsigns").reset_hunk() end, desc = "Git: reset hunk" },
-			{ "<leader>gb", function() require("gitsigns").blame_line() end, desc = "Git: blame line" },
-			{ "<leader>gB", function() require("gitsigns").blame_line({ full = true }) end, desc = "Git: blame full" },
-
-			{
-				"<leader>gs",
-				function() 
-					local opts = {}
-
-					-- Env variables GIT_WORK_TREE and GIT_DIR solves problems with not finding git still need conditional
-					-- because of expanding files with -u tries to read every file on $HOME
-					if vim.env.GIT_WORK_TREE == vim.env.HOME then
-						opts.expand_dir = false
-					end
-
-					require("telescope.builtin").git_status(opts)
-				end,
-				desc = "Git: status"
-			},
-			{ "<leader>gc", ":Git commit<cr>", desc = "Git: commit" },
-			{ "<leader>g!", ":Git commit --amend<cr>", desc = "Git: commit with amend" },
-		},
+		config = function() require("config.git") end,
 	},
 
 	{
