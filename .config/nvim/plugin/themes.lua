f388b0d7855d51de37ed4e12b0756589b83bfd95
Daniel Santos Dantas <daniel@sdantas.dev>
2024-05-29 22:37:33 -0300

    set theme on plugin definition

--- .config/nvim/plugin/themes.lua
+++ .config/nvim/plugin/themes.lua
@@ -1,4 +1,2 @@
 vim.opt.termguicolors = true
 vim.opt.background = "dark"
-vim.cmd.highlight("clear") -- needs to be set before colorscheme https://github.com/folke/lazy.nvim/issues/40
-vim.cmd.colorscheme("tokyonight")
