76c0021c5be07a2d2e593429a12de055876fca82
Daniel Santos Dantas <daniel@sdantas.dev>
2024-07-26 09:54:50 -0300

    update command for building

--- .config/nvim/lua/plugins_not_vscode/telescope.lua
+++ .config/nvim/lua/plugins_not_vscode/telescope.lua
@@ -10,7 +10,7 @@ return {
 			{ "fdschmidt93/telescope-egrepify.nvim", dependencies = "nvim-lua/plenary.nvim" },
 			{
 				"nvim-telescope/telescope-fzf-native.nvim",
-				build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+				build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
 			},
 		},
 	},
