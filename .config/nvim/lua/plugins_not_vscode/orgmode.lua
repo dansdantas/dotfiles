0c0079f66ebe8925c857d45f2003502a7ed01460
Daniel Santos Dantas <daniel@sdantas.dev>
2025-11-25 14:24:34 -0300

    move orgmode keybinds

--- .config/nvim/lua/plugins_not_vscode/orgmode.lua
+++ .config/nvim/lua/plugins_not_vscode/orgmode.lua
@@ -6,11 +6,11 @@ return {
 		org_agenda_files = vim.fn.stdpath("data") .. "/orgfiles/**/*",
 		org_default_notes_file = vim.fn.stdpath("data") .. "/orgfiles/refile.org",
 		org_todo_keywords = { "TODO", "WAITING", "|", "DONE", "DELEGATED" },
-		mappings = { prefix = ",o" },
+		mappings = { prefix = ",og" },
 	},
 	keys = {
 		{
-			",of",
+			",ogf",
 			function() Snacks.picker.files({ cwd = vim.fn.stdpath("data") .. "/orgfiles/" }) end,
 			desc = "Telescope: orgfiles",
 		},
