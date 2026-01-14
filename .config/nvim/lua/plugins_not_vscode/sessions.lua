a1724cdfa82c1d84f965e002bb82ba02f1902d44
Daniel Santos Dantas <daniel@sdantas.dev>
2026-01-14 07:55:45 -0300

    enable types when possible

--- .config/nvim/lua/plugins_not_vscode/sessions.lua
+++ .config/nvim/lua/plugins_not_vscode/sessions.lua
@@ -1,6 +1,10 @@
 return {
 	"rmagatti/auto-session",
 	lazy = false,
+
+	---enables autocomplete for opts
+	---@module "auto-session"
+	---@type AutoSession.Config
 	opts = {
 		suppressed_dirs = { "~/projects" },
 		session_lens = {
